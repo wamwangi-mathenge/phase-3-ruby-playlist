3ebc303b529429df67d1c7a302b2943dc5dbdd4d
Brian <wamwangimathenge@gmail.com>
2023-02-10 09:58:58 +0300

    Media for loop

--- Media.rb
+++ Media.rb
@@ -7,10 +7,27 @@ end
 
 class Song < Media
     attr_accessor :album, :artist
+
+    def intialize(attributes)
+        attributes.each do |key, value|
+            # self.class.attr_accessor(key)
+            self.send("#{key}=", value)
+    end
 end
 
 class Podcast < Media
     attr_accessor :host
     def review()
     end
+end
+
+collection = []
+
+library = []
+
+for song in librart do
+    # Create song from the Song class
+    item = Song.new(**song)
+    # collection.push(item)
+    puts item
 end
