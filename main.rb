1df3e06d6adce449657e8d23c4e7224d5aa9a559
Brian <wamwangimathenge@gmail.com>
2023-02-10 15:18:22 +0300

    catalogue file

---
+++ main.rb
@@ -0,0 +1,53 @@
+library = []
+
+catalogue = [
+    {
+        name: "Niggas in Paris",
+        duration: "4:50",
+        genre: "Hip-Hop",
+        album: "Watch the Throne",
+        year: 2011,
+        artist: "Kanye West ft. Jay Z"
+    },
+    {
+        name: "The Box",
+        duration: "4:50",
+        genre: "Hip-Hop",
+        album: "Please Excuse Me for Being Antisocial",
+        year: 2019,
+        artist: "Roddy Rich"
+    },
+    {
+        name: "Niggas in Paris",
+        duration: "4:50",
+        genre: "Hip-Hop",
+        album: "Watch the Throne",
+        year: 2011,
+        artist: "Kanye West ft. Jay Z"
+    },
+    {
+        name: "Niggas in Paris",
+        duration: "4:50",
+        genre: "Hip-Hop",
+        album: "Watch the Throne",
+        year: 2011,
+        artist: "Kanye West ft. Jay Z"
+    },
+    {
+        name: "Niggas in Paris",
+        duration: "4:50",
+        genre: "Hip-Hop",
+        album: "Watch the Throne",
+        year: 2011,
+        artist: "Kanye West ft. Jay Z"
+    },
+]
+
+for item in catalogue do
+    # Create song from the Song class
+    song = Song.new(**song)
+    # collection.push(item)
+    puts item
+end
+
+puts collection
